ac60b685e80a1f98505a6003836f3d62624266f8
ahsansaeed202 <ahsansaeed7t@gmail.com>
2024-07-25 00:37:44 +0500

    revisions

--- src/Components/Privacy/index.jsx
+++ src/Components/Privacy/index.jsx
@@ -144,8 +144,8 @@ const PrivacyModal = () => {
                             Strictly Necessary Cookies
                         </AccordionSummary>
                         <AccordionDetails>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        These cookies are necessary for the website to function and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences, logging in or filling in forms. You can set your browser to block or alert you about these cookies, but some parts of the site will not then work.<br />
+                        <a href="/">Cookies details</a>
                         </AccordionDetails>
                     </Accordion>
                     <Accordion>
@@ -157,8 +157,8 @@ const PrivacyModal = () => {
                             Performance Cookies
                         </AccordionSummary>
                         <AccordionDetails>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies we will not know when you have visited our site, and will not be able to monitor its performance. <br /> 
+                        Cookies details
                         </AccordionDetails>
                     </Accordion>
                     <Accordion>
@@ -170,8 +170,7 @@ const PrivacyModal = () => {
                             Targeting Cookies
                         </AccordionSummary>
                         <AccordionDetails>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies we will not know when you have visited our site, and will not be able to monitor its performance. <br /> Cookies details
                         </AccordionDetails>
                     </Accordion>
                 </Box>
